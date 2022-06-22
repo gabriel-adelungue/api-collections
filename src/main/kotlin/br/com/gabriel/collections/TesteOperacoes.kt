8b5a6fc5a408fb56a8cd9ca3c281c2d59e5bf8ec
Gabriel Adelungue <40636838+gabriel-adelungue@users.noreply.github.com>
2022-06-22 13:34:43 -0300

    updated operations of test class

--- src/main/kotlin/br/com/gabriel/collections/TesteOperacoes.kt
+++ src/main/kotlin/br/com/gabriel/collections/TesteOperacoes.kt
@@ -18,4 +18,16 @@ fun main() {
     salariosMaiorQue2500.forEach {
         println(it)
     }
+
+    println("----------------------------")
+    println(salarios.count { it in  2000.0 .. 5000.0})
+
+    println("----------------------------")
+    println(salarios.find { it ==  2250.0})
+    println(salarios.find { it ==  900.0})
+
+    println("----------------------------")
+    println(salarios.any { it ==  1000.0})
+    println(salarios.any { it ==  500.0})
+
 }
